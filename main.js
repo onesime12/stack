5eae6467af2099b984f4432c51caf916047722da
onesime12 <wanzalireonesime@gmail.com>
2020-05-30 11:35:21 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const Stack=require("./stack");
+
+const stack=new Stack();
+
+
+stack.push(100)
+stack.push(200)
+stack.push(300)
---
+++ stack.js
@@ -0,0 +1,26 @@
+class Stack{
+    constructor(){
+        this.table=[];
+        this.count=0;
+    }
+
+    push(data){
+        this.table[this.count]=data;
+        console.log(this.table[this.count]);
+        this.count++;
+    }
+
+    pop(){
+        this.table[this.count--];
+        console.log(this.table)
+    }
+}
+
+const stack=new Stack();
+
+stack.push(100)
+stack.push(200)
+stack.push(300)
+stack.pop()
+
+module.exports={Stack};
